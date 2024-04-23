7c4691345b5ece1cb4c36f5f883d8c8b086a72a2
sujaltrivedi26 <167772206+sujaltrivedi26@users.noreply.github.com>
2024-04-23 12:09:44 +0000

    01_variables,02_datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 143234
+let accountEmail= " sujal@google.com"
+var accountpassword="854844785"
+accountCity = "vadodara"
+let accountState;
+
+
+// accountId= 2 not allowed
+accountEmail = "sujal@2234.com"
+accountpassword = "849304"
+accountCity ="Canada"
+console.log(accountId); 
+
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+ "use strict"; // treat all JS code as newer version
+
+ // alert(3+3)// we are using nodejs, not browser 
+
+ console.log(3+3)
+
+ console.log("sujal")
+
+
+ let name =" sujal"
+ let age= 15
+ let isLoggedIn = false 
+ let state;
+
+ // number => 2 to power 53
+ // bigint 
+ // string => true/false
+ // null => standalone value
+ // undefined => is type of same undefined 
+ // string =>""
+ // symbol=> unique
+ // object
+
+ console.log(typeof null);
